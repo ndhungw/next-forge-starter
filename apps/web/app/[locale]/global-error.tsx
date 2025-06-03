423d6245658703bf3a3636dd4e1604f25f062f7a
HungND <hungnd137@gmail.com>
2025-06-03 22:02:46 +0700

    chore: bump-deps

--- apps/web/app/[locale]/global-error.tsx
+++ apps/web/app/[locale]/global-error.tsx
@@ -2,7 +2,6 @@
 
 import { Button } from '@repo/design-system/components/ui/button';
 import { fonts } from '@repo/design-system/lib/fonts';
-import { captureException } from '@sentry/nextjs';
 import type NextError from 'next/error';
 import { useEffect } from 'react';
 
@@ -13,7 +12,9 @@ type GlobalErrorProperties = {
 
 const GlobalError = ({ error, reset }: GlobalErrorProperties) => {
   useEffect(() => {
-    captureException(error);
+    // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+    // biome-ignore lint/suspicious/noConsole: <explanation>
+    console.log(error);
   }, [error]);
 
   return (
